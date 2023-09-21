a891de4171ff2b0b2ee24f9d4f12a4e86c082276
Abhiram H <abhiramh92@gmail.com>
2023-09-21 21:18:05 +0530

    modified:   lua/settings/init.lua

--- lua/settings/init.lua
+++ lua/settings/init.lua
@@ -54,7 +54,7 @@ local options = {
     equalalways = false,
     breakindent = true,
     hlsearch = false,
-    
+
 }
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
@@ -76,6 +76,5 @@ vim.g.completeopt = "menu,menuone,noselect,noinsert"
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 -- vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
--- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
--- vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
--- vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+-- vim.keymap.set('n', '[d', vim
+
