5ad16ae184276bed610e3fd2f63922e5638f3a8c
Abhiram H <abhiramh92@gmail.com>
2024-01-07 21:29:04 +0530

    Signed-off-by: Abhiram H <abhiramh92@gmail.com>
    	modified:   init.lua

--- init.lua
+++ init.lua
@@ -11,6 +11,16 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
-pcall(require, "settings")
-require("lazy").setup("plugins")
-pcall(require, "keybindings")
+local status_ok = pcall(require, "settings")
+if not status_ok then
+  vim.notify("settings not load")
+end
+local status_ok1 = require("lazy").setup("plugins")
+if not status_ok1 then
+  vim.notify("plugins not load")
+end
+local status_ok2 = pcall(require, "keybindings")
+
+if not status_ok2 then
+  vim.notify("keybindings not load")
+end
