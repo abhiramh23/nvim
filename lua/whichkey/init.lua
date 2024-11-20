69f05815f4e5b361c9b098404764d5e300803934
Abhiram H <abhiramh92@gmail>
2024-11-20 10:37:42 +0530

    modified:   lua/whichkey/init.lua

--- lua/whichkey/init.lua
+++ lua/whichkey/init.lua
@@ -1,11 +1,35 @@
-local wk = require("which-key")
-local config_path = "~\\AppData\\Local\\nvim\\init.lua"
+-- Function to determine the operating system
+local function get_os()
+    if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+        return "windows"
+    elseif vim.fn.has("unix") == 1 then
+        return "linux"
+    else
+        return "unknown"
+    end
+end
 
+-- Determine the OS and set the config path
+local os_name = get_os()
+local config_path
+
+if os_name == "windows" then
+    config_path = "~\\AppData\\Local\\nvim\\init.lua"
+elseif os_name == "linux" then
+    config_path = "~/.config/nvim/init.lua"
+else
+    error("Unsupported operating system")
+end
+
+-- Load which-key
+local wk = require("which-key")
+
+-- Define key mappings with which-key
 wk.add({
-    { "<leader>E",  "<cmd>e" .. config_path .. " <cr>", desc = "Edit Configuration" },
+    { "<leader>E",  "<cmd>e " .. vim.fn.fnameescape(config_path) .. " <cr>", desc = "Edit Configuration" },
     { "<leader>f",  group = "file" }, -- group
-    { "<leader>ff", "<cmd>Telescope find_files<cr>",    desc = "Find File",         mode = "n" },
-    { "<leader>fn", desc = "New File" }, 
+    { "<leader>ff", "<cmd>Telescope find_files<cr>",                         desc = "Find File",         mode = "n" },
+    { "<leader>fn", desc = "New File" },
     {
         "<leader>b",
         group = "buffers",
@@ -22,5 +46,4 @@ wk.add({
         { "<leader>w", "<cmd>w<cr>",  desc = "Write" },
         { "<leader>Q", "<cmd>wq<cr>", desc = "Write Quit" },
     }
-
 })
